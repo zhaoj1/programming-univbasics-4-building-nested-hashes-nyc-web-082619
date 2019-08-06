1226326cd8c7c9907ab8deb03c9e3630b1210f5d
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:36:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,10 +26,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :lvl1 => 25,
-        :lvl2 => 50,
-        :lvl3 => 75,
-        :lvl4 => 100
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 75,
+        :four_pieces_owned => 100
       },
       :names => {
         :LIRR => {},
