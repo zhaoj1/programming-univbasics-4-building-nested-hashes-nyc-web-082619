d4de2a4698c2882de5386dca37045be330c57859
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:42:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,7 +64,7 @@ def monopoly_with_fourth_tier
           "mortgage_value" => "$200"
         }, 
         :b_and_o_railroad => {
-          "mortgage_value" => "400"
+          "mortgage_value" => "$400"
         }, 
         :shortline_railroad => {
           "mortgage_value" => "$"
