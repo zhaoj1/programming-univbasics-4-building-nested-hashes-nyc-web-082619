d05dd28074685e952a565b1ac6ae5a36a84f66e8
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:09:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	my_hash = {:railroads => ()}
+	my_hash = {:railroads => {}}
 	my_hash
 end
 
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly = {:railroads => ()}
+  monopoly = {:railroads => {}}
   monopoly
 end
 
