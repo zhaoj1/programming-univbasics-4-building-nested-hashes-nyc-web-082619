3d0c672f163ed250b11238477c1bf404d93a343b
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:09:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,7 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	my_hash = {:railroads => "RR1"}
+	my_hash = {:railroads => ()}
 	my_hash
 end
 
