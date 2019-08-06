362f0305739257203531f50918b82cdc62f82914
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:24:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,10 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  monopoly_with_third_tier =
+  monopoly_with_third_tier = {:railroads => {
+    :pieces => 4,
+    :rent_in_dollars => 1
+  }
   monopoly_with_third_tier
 end
 
