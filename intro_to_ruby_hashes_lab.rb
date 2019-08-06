1474df979ff92b190c07a3fd0f5ec9966ed82b79
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:40:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,7 +58,7 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {
-          "mortgage value" = "$100"
+          "mortgage_value" = "$100"
         },
         :pennsylvania_railroad => {}, 
         :b_and_o_railroad => {}, 
