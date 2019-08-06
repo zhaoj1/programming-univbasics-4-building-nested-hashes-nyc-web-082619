9df71751427d5768f6d0c1aa25b982f73d6ded80
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-06 20:38:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
         :reading_railroad => {},
         :pennsylvania_railroad => {}, 
         :b_and_o_railroad => {}, 
-        :Amtrak => {}
+        :shortline_railroad => {}
       }
     }
   }
